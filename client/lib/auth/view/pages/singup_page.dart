a10ba961193a3dc66cb7b1084ef50a00ad3641f9
allanbastian <allan.bastian9.ab@gmail.com>
2024-06-26 02:36:06 +0530

    feat: added app pallete

---
+++ client/lib/auth/view/pages/singup_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+
+  @override
+  void dispose() {
+    
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
