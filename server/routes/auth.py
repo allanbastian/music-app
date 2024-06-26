0dcaf81214fbeb1d50d47227c4187d9d6f9eccf2
allanbastian <allan.bastian9.ab@gmail.com>
2024-06-26 08:24:58 +0530

    feat: added API for getting authenicated user details

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,8 +1,9 @@
 import uuid
 import bcrypt
 import jwt
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, Header
 from database import get_db
+from middleware.auth_middleware import auth_middleware
 from models.user import User
 from pydantic_schemas.user_create import UserCreate
 from fastapi import APIRouter
@@ -44,3 +45,10 @@ def login_user(user: UserLogin, db: Session=Depends(get_db)):
     token = jwt.encode({'id': user_db.id}, 'password_key')
     
     return {'token':token, 'user':user_db}
+
+@router.get('/')
+def current_user_data(db: Session=Depends(get_db), user_dict = Depends(auth_middleware)):
+    user = db.query(User).filter(User.id == user_dict['uid']).first()
+    if not user:
+        raise HTTPException(404, 'User not found!')
+    return user
